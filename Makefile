09e4f6b9f75b59edeae2135b79e4cff977730f71
bill <bill@bill-Satellite-L500.(none)>
2013-07-07 11:32:32 +0800

    Sorting code done; OpenMP-enabled code

--- Makefile
+++ Makefile
@@ -1,7 +1,10 @@
 default:cloudless
 
 cloudless:cloudless.cpp
-	g++ cloudless.cpp -o cloudless -I/usr/local/include `pkg-config opencv --cflags --libs`
+	g++ cloudless.cpp -o cloudless -I/usr/local/include \
+		`pkg-config opencv --cflags --libs` \
+		-fopenmp -lgomp
+	export OMP_NUM_THREADS=2
 fixpath:
 	sudo ldconfig -v
 	echo "export LD_LIBRARY_PATH=/usr/local/lib:$LD_LIBRARY_PATH"
--- cloudless.cpp
+++ cloudless.cpp
@@ -22,10 +22,13 @@
 */
 
 #include <cmath>
+#include <cstdlib>
 #include <iostream>
+#include <cassert>
 #include <opencv/cv.h>
 #include <opencv/highgui.h>
 #include <opencv2/opencv.hpp>
+#include <omp.h>
 
 #define MAX_IMG (366*2)
 
@@ -36,6 +39,18 @@ int from_day, to_day, total_days;
 int total_image;
 Mat img[MAX_IMG];
 Mat mark[MAX_IMG];
+
+#define numChannel 3
+typedef struct data_s{
+  uint8_t mark;
+  uint8_t img_data[numChannel];
+} data_t;
+data_t* data;
+
+int pixelComparator(const void* a, const void* b)
+{
+  return (((data_t*)b)->mark) - (((data_t*)a)->mark);
+}
 
 void combineImages(Mat &dst)
 {
@@ -61,6 +76,7 @@ void combineImages(Mat &dst)
 
 int main( int argc, char** argv )
 {
+//STAGE: Read arguments
     char day[3];
     string filename;
     Mat output;
@@ -74,7 +90,8 @@ int main( int argc, char** argv )
     to_day = atoi(argv[3]);
     total_days = to_day - from_day + 1;
 
-    // Read images
+//STAGE: Read images
+printf("Reading images...\n");
     int i,j;
     total_image = 0;
     for (int i=from_day;i<=to_day;i++) {
@@ -101,7 +118,8 @@ int main( int argc, char** argv )
         }
     }
 
-    // Processing should be here
+//STAGE: Give marks
+printf("Giving marks to pixels...\n");
     // First we identify NULL/Data-invalid pixels.
     // These pixels are given zero marks directly.
     Mat lookUpTable(1, 256, CV_8U);
@@ -109,8 +127,11 @@ int main( int argc, char** argv )
     for( int i = 0; i < 256; ++i) {
         p[i] = 255;
     }
-    p[0] = p[255] = 0;
+    for( int i = 0; i < 5; ++i) {
+      p[i] = p[255-i] = 0;
+    }
     
+    #pragma omp parallel for private(i)
     for (int i=0;i<total_image;i++) {
         Mat gray = Mat(img[i]);
         cvtColor( img[i], gray, CV_RGB2GRAY );
@@ -119,6 +140,7 @@ int main( int argc, char** argv )
 
     // Then we give marks to remaining pixels...
     // Depending on the S value in the HSV colorspace.
+    #pragma omp parallel for private(i)
     for (int i=0;i<total_image;i++) {
         Mat hsv = Mat(img[i].rows, img[i].cols, CV_8UC1, 0); // TODO: Type is hard-coded!
         Mat channel[3];
@@ -127,10 +149,49 @@ int main( int argc, char** argv )
         }
         cvtColor( img[i], hsv, CV_RGB2HSV);
         split( hsv, channel );
-        mark[i] = mark[i].mul(channel[1], 1/255.0f);
+        mark[i] = mark[i].mul(channel[1], 1/255.0f); // S channel
         //img[i] = mark[i];
     }
 
+//STAGE: Sort pixels
+printf("Sorting pixels...\n");
+    int row = img[0].rows, col = img[0].cols;
+    // Testing parallelization of the loop   
+    #pragma omp parallel for private(i,j,data)
+    for (int i=0;i<row;i++) {
+        for (int j=0;j<col;j++) {
+            // Initialize the matrix and extract data from arrays img and data
+            data = (data_t*) malloc(sizeof(data_t)*total_image);
+            for (int k=0;k<total_image;k++) {
+                if(img[k].data){
+                  data[k].mark = ((uint8_t*)(mark[k].data))[i*col+j];
+                  // Unroll it might be slightly better for performance
+                  data[k].img_data[0] = ((uint8_t*)(img[k].data))[i*col*numChannel+j*numChannel+0];
+                  data[k].img_data[1] = ((uint8_t*)(img[k].data))[i*col*numChannel+j*numChannel+1];
+                  data[k].img_data[2] = ((uint8_t*)(img[k].data))[i*col*numChannel+j*numChannel+2];
+                }
+            } // end for(k)
+            // Sorting procedure
+            qsort(data,total_image,sizeof(data_t),pixelComparator);
+            //Putting data back
+            for (int k=0;k<total_image;k++) {
+                if(img[k].data){
+                  ((uint8_t*)(mark[k].data))[i*col+j] = data[k].mark;
+                  // Unroll it might be slightly better for performance
+                  ((uint8_t*)(img[k].data))[i*col*numChannel+j*numChannel+0] = data[k].img_data[0];
+                  ((uint8_t*)(img[k].data))[i*col*numChannel+j*numChannel+1] = data[k].img_data[1];
+                  ((uint8_t*)(img[k].data))[i*col*numChannel+j*numChannel+2] = data[k].img_data[2];
+                }else{
+                  // If this is invoked, that means we will have to add code for skipping the null img[].data
+                  assert(1);
+                }
+            } // end for(k)            
+            free(data);
+        } // end for(j)
+    } // end for(i)
+
+//STAGE: Output
+printf("Generating output...\n");
     // Generating "Thumbnail" for reference
     combineImages(output);
     imwrite("Output.jpg", output);
