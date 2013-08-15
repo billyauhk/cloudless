17aab37e661c148497ffc0145f7a0f43f30a6371
Forte <forte@ieee.org>
2013-08-15 12:08:16 +0900

    Added incomplete renderMODIS

--- Makefile
+++ Makefile
@@ -1,21 +1,26 @@
+CC=g++
+CFLAGS=-O2
+
 default:cloudless
 
 cloudless:cloudless.cpp
-	g++ cloudless.cpp -O2 -o cloudless -I/usr/local/include \
+	$(CC) $(CFLAGS) cloudless.cpp -o cloudless -I/usr/local/include \
 		`pkg-config opencv --cflags --libs` \
 		-fopenmp -lgomp
 	export OMP_NUM_THREADS=4
 charlie:cloudless.cpp
-	g++ cloudless.cpp -O2 -o charlie -I/usr/local/include \
+	$(CC) $(CFLAGS) cloudless.cpp -o charlie -I/usr/local/include \
 		`pkg-config opencv --cflags --libs` \
 		-fopenmp -lgomp -DORIGINAL
 	export OMP_NUM_THREADS=4
 tiff:tiff.cpp
-	g++ tiff.cpp -O2 -o tiff -I/usr/local/include \
+	$(CC) $(CFLAGS) tiff.cpp -o tiff -I/usr/local/include \
 		`pkg-config opencv --cflags --libs`
 version:version.cpp
-	g++ version.cpp -O2 -o tiff -I/usr/local/include \
+	$(CC) $(CFLAGS) version.cpp -o tiff -I/usr/local/include \
 		`pkg-config opencv --cflags --libs`
+renderMODIS:renderMODIS.cpp
+	$(CC) $(CFLAGS) renderMODIS.cpp -o renderMODIS
 fixpath:
 	sudo ldconfig -v
 	echo "export LD_LIBRARY_PATH=/usr/local/lib:$LD_LIBRARY_PATH"
--- renderMODIS.cpp
+++ renderMODIS.cpp
@@ -1,32 +1,119 @@
-#include<cstdio>
-#include<cstdlib>
-#include<iostream>
-#include<cstring>
-
 /*
-  Expected invocation:
-  Specifying year and day: -start 2012001 -stop 2012366
-  Specifying location: -row 12 -col 32 (default r12c32)
-  Specifying resolution: -res 2km (default 2km)
-  NO need to specify satellite name (use wildcard try both "aqua" and "terra")
+ Cloudless Atlas
+ 
+ Usage: renderMODIS [folder path] [pattern] [resource file]
+ Example: ./renderMODIS Images/ *.jpg resource.txt
+ 
+ */
 
-  Specifying interactiveness: +i (need GUI), -i (no GUI)
-  
-*/
+#include <iostream>
+#include <cstring>
+#include <sys/stat.h>
+#include <unistd.h>
+#include <cmath>
+#include <cctype>
+#include <cerrno>
+#include <dirent.h>
+#include <climits>
+#include <glob.h>
+
+#define MACHINE_MAX 100
+#ifndef HOST_NAME_MAX
+    #define HOST_NAME_MAX 255
+#endif
 
 using namespace std;
 
-int main(int argc, char* argv[]){
-  string filePath;
-  
-  // Pattern for reference: Images/RRGlobal_r12c32.2012001.aqua.2km.jpg
-  string folderPath="Images/";
-  string filePrefix="RRGlobal_";
-  string fileSuffix=".jpg";
+typedef struct stat stat_t;
+typedef struct
+{
+    char hostname[HOST_NAME_MAX];
+    int cpu_core; // Number of CPU core
+    int memory; // Total memory size in MB
+} machine_t;
 
-  filename = string("Images/RRGlobal_r12c32.2012001.aqua.2km.jpg");
-  cout << filename << endl;
-
-
-  return 0;
+void matchFilePattern(char * pattern, off_t * file_size, long int * total_image)
+{
+    // ---------------------------------------------------------------
+    // This function does the followings:
+    // (1)parse files according to specified pattern
+    // (2)set file_size, total_image
+    // ---------------------------------------------------------------
+    
+    glob_t globbuf;
+    stat_t statbuf;
+    
+    globbuf.gl_offs = 0;
+    glob(pattern, GLOB_DOOFFS, NULL, &globbuf);
+    if (globbuf.gl_pathc == 0) {
+        *total_image = 0;
+        printf("No files match the pattern under this path.\n");
+        exit(EXIT_FAILURE);
+    } else {
+        if (stat(globbuf.gl_pathv[0], &statbuf) == 0) {
+            *file_size = statbuf.st_size;
+        }
+        *total_image = globbuf.gl_pathc;
+    }
 }
+
+void parseResourceFile(char resource_file[], machine_t * machine, int * total_machine)
+{
+    // ---------------------------------------------------------------
+    // This function does the followings:
+    // (1)reads the specified resource file
+    // ---------------------------------------------------------------
+    
+    FILE * fd;
+    int i;
+
+    if ((fd = fopen(resource_file, "r")) == NULL) {
+        perror(resource_file);
+        exit(EXIT_FAILURE);
+    }
+    for (i = 0; (fscanf(fd, "%s %d %d", machine[i].hostname, &machine[i].cpu_core, &machine[i].memory)) != EOF; i++);
+    *total_machine = i;
+    fclose(fd);
+}
+
+void jobsAlloc(machine_t machine[], off_t file_size, int total_machine, long int total_image)
+{
+    // ---------------------------------------------------------------
+    // This function does the followings:
+    // (1)run the jobs allocation algorithm according to CPU and RAM
+    // (2)output a mapping of jobs and handling machines
+    // ---------------------------------------------------------------
+}
+
+int main(int argc, const char * argv[])
+{
+    char folder_path[PATH_MAX], pattern[PATH_MAX], resource_file[PATH_MAX];
+    int total_machine;
+    long int total_image;
+    machine_t machine[MACHINE_MAX];
+    off_t file_size; // File size in unit of Byte(B)
+    
+    if (argc != 4) {
+        printf("Usage: %s [folder path] [pattern] [resource file]\n", argv[0]);
+        exit(EXIT_FAILURE);
+    }
+    strcpy(folder_path, argv[1]);
+    strcpy(pattern, argv[2]);
+    strcpy(resource_file, argv[3]);
+    
+    parseResourceFile(resource_file, machine, &total_machine);
+    
+    // Move to the image folder
+    if (chdir(folder_path)) {
+        perror(folder_path);
+        exit(EXIT_FAILURE);
+    }
+    
+    matchFilePattern(pattern, &file_size, &total_image);
+    
+    jobsAlloc(machine, file_size, total_machine, total_image);
+    
+    return 0;
+}
+
+
